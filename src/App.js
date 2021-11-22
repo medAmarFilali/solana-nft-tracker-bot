31029bd331239eb950ba68fdd72edb10ba053313
Amar Filali <medamarfilai@gmail.com>
2021-11-22 12:34:29 +0100

    Added nft key form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import { Typography, Button } from "@mui/material";
+import { useState } from "react";
+import { Typography, Container, TextField } from "@mui/material";
 import { makeStyles } from "@mui/styles";
-import { grey } from "@mui/material/colors";
 
 const App = () => {
   const classes = useStyles();
+  const [nftKey, setNftKey] = useState("");
   return (
     <div className={classes.pageContainer}>
-      <Typography variant="subtitle2">Hello World</Typography>
-      <Button variant="contained" color="primary">
-        Salut tout le monde
-      </Button>
+      <Container maxWidth="xl">
+        <div className={classes.headerContainer}>
+          <div className={classes.formContainer}>
+            <Typography variant="h6" sx={{ textAlign: "center" }}>
+              Enter your NFT Key
+            </Typography>
+            <TextField
+              variant="outlined"
+              value={nftKey}
+              onChange={(e) => setNftKey(e.target.value)}
+              label="NFT KEY"
+              sx={{ marginTop: 2, width: "600px" }}
+              fullWidth
+            />
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
 
 const useStyles = makeStyles((theme) => ({
   pageContainer: {
-    backgroundColor: grey[100],
+    // backgroundColor: grey[100],
+  },
+  formContainer: {
+    display: "flex",
+    flexDirection: "column",
+    marginTop: theme.spacing(8),
+  },
+  headerContainer: {
+    display: "flex",
+    justifyContent: "center",
   },
 }));
 
