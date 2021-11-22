e4961a1b0626113b531fa60f830088eace2be315
Amar Filali <medamarfilai@gmail.com>
2021-11-22 12:21:13 +0100

    Tested Theming

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { Typography } from "@mui/material";
+import { Typography, Button } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { grey } from "@mui/material/colors";
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.pageContainer}>
       <Typography variant="subtitle2">Hello World</Typography>
+      <Button variant="contained" color="primary">
+        Salut tout le monde
+      </Button>
     </div>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  pageContainer: {
+    backgroundColor: grey[100],
+  },
+}));
 
 export default App;
